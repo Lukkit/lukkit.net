bdaf272f6752ba9741c3adc79283389cf26e1622
mathhulk <no-reply@mathhulk.com>
2019-01-17 18:01:16 -0800

    Default dark theme and theme selection

--- resources/javascript/lukkit.js
+++ resources/javascript/lukkit.js
@@ -1,5 +1,13 @@
+if(Cookies.get("theme") === "light") {
+	$("body").removeClass("dark");
+} else {
+	Cookies.set("theme", "dark");
+}
+
 $(document).ready(function( ) {
 	$(".toggle").click(function( ) {
 		$("body").toggleClass("dark");
+		
+		Cookies.set("theme", $("body").hasClass("dark") ? "dark" : "light");
 	});
 });
