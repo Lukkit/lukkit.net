f8677221e3f74cbdf006a0c5adff64f11c7f5a71
mathhulk <no-reply@mathhulk.com>
2019-01-17 18:12:09 -0800

    Replace cookies with localStorage

--- resources/javascript/lukkit.js
+++ resources/javascript/lukkit.js
@@ -1,13 +1,13 @@
-if(Cookies.get("theme") === "light") {
+if(store.get("theme") === "light") {
 	$("body").removeClass("dark");
 } else {
-	Cookies.set("theme", "dark");
+	store.set("theme", "dark");
 }
 
 $(document).ready(function( ) {
 	$(".toggle").click(function( ) {
 		$("body").toggleClass("dark");
 		
-		Cookies.set("theme", $("body").hasClass("dark") ? "dark" : "light");
+		store.set("theme", $("body").hasClass("dark") ? "dark" : "light");
 	});
 });
